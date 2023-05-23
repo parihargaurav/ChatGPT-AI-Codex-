58e68e88db8a8253f5e75935fb09a3f1ddb22a6b
Gaurav <parihargaurav527@gmail.com>
2023-05-23 13:41:19 +0530

    update the client url

--- client/script.js
+++ client/script.js
@@ -111,7 +111,7 @@ const handleSubmit = async (e) => {
     const data = await response.json();
     const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
-    console.log({parsedData})
+    console.log({ parsedData })
 
     typeText(messageDiv, parsedData)
   } else {
