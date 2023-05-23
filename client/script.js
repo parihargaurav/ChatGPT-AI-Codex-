8636fb76acba99f20bde3de94e916d0d4706a389
Gaurav <parihargaurav527@gmail.com>
2023-05-23 13:28:02 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -94,7 +94,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-0eca.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
